eb29b80a80fa3bf898a8974e0f142bbd530f65cc
Eric M. Johnson <eric.johnson@housecallpro.com>
2018-12-02 20:14:34 -0800

    Basic project setup

---
+++ lib/day_01.rb
@@ -0,0 +1,5 @@
+class Day01
+  def self.run
+    return true
+  end
+end
---
+++ spec/day_01_spec.rb
@@ -0,0 +1,7 @@
+require 'day_01'
+
+describe Day01, '#run' do
+  it 'always returns a truthy value' do
+    expect(Day01.run).to be_truthy
+  end
+end
